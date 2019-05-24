39590026d2090cb6e951fdac8db984d589ed9f0f
Kenny Baas <kenny@baasie.com>
2019-05-24 16:35:48 +0200

    Added upcoming events

--- src/Data/Queries.fs
+++ src/Data/Queries.fs
@@ -5,22 +5,25 @@ open App.Types
 
 let sessions =
     [
-//        Upcoming_session {
-//              title = "Live stream of DDD Budapest CraftConf meetup with Nick Tune and Helin Akgul"
-//              date = "Wednesday, May 8th. 2019 - 18:30 GMT"
-//              img = None
-//              video = Some "https://www.ustream.tv/recorded/121763272"
-//              embedded = None
-//              description = "Nick Tune is coming to talk at Craft Conference (yaaay!!) and whilst he is here in Budapest he brings us one of his future talks (Strategic Microservice Patterns - find details below) and present it on this meetup, so DO NOT MISS IT :)
-//                              I am also pleased to announce another great presenter Helin Akgul. She will be talking about DDD practices in TransferWise, and she will be giving some examples around how they are using product engineering to create domain experts, and tips and tricks to adopting DDD practices gradually.
-//                              I will post the abstract of her talk very soon.
-//                              "
-//              links =
-//                  [
-//                      { label = "The event on Meetup" ; url = "https://www.meetup.com/Virtual-Domain-Driven-Design-meetup/events/261134675/" }
-//                      { label = "Hosted by DDD Budapest" ; url = "https://www.meetup.com/DDD-Practitioners-in-Budapest/events/260694621/" }
-//                  ]
-//                  }
+        Upcoming_session {
+              title = "SunDDDay discussion: EventStorming and User story mapping for domain discovery"
+              date = "Sunday, May 26th. 2019 - 15:30 GMT / 16:30 CET"
+              img = Some "https://secure.meetupstatic.com/photos/event/6/f/a/2/highres_477448578.jpeg"
+              video = None
+              embedded = None
+              description = "On this first SunDDDay 26th May at 16:30 Central European Time (Amsterdam GMT +2), virtual DDD meetup will hold an online panel discussion where you can ask questions!
+
+                             Marco Heimeshoff, and Kenny baas-Schwegler (with a possible attendance by Zsófia Herendi and Trond Hjorteland) will discuss their experience with EventStorming and User story mapping for domain discovery.
+
+                            The two main questions are:
+                            * How can we best combine User Story Mapping and EventStorming for domain discovery.
+                            * How can we go from EventStorming to user stories.
+                              "
+              links =
+                  [
+                      { label = "The event on Meetup" ; url = "https://www.meetup.com/Virtual-Domain-Driven-Design-meetup/events/257656741/" }
+                  ]
+                  }
         
         Past_session
           {
