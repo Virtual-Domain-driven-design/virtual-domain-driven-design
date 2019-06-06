493d3744d0b95d03e6a614bd07d6906d416924b5
Kenny Baas <kenny@baasie.com>
2019-06-06 07:41:23 +0200

    Update package and organisers

--- src/Main.fs
+++ src/Main.fs
@@ -8,11 +8,13 @@ open Fable.Core.JsInterop
 open App.Types
 open App.Interop
 open App.Data
+open App.Views
 open App.Views.Navigation
 open App.Views.CodeOfConduct
 open App.Views.Hero
 open App.Views.About
 open App.Views.Sessions
+open App.Views.Organisers
 open App.Views.Footer
 
 importAll "./style.css"
@@ -48,7 +50,8 @@ let landingpage model dispatch =
   div []
     [ hero model dispatch
       about model dispatch
-      sessions model dispatch ]
+      sessions model dispatch
+      organisers model dispatch ]
 
 
 let view (model:Model) dispatch =
