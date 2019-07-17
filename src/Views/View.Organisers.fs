adec747437eaec5875b71b74496649b1711c64ac
Marco Heimeshoff <heimeshoff@googlemail.com>
2019-07-17 12:32:14 +0200

    Organisers: Animated Card, redesigned, code-cleanup

--- src/Views/View.Organisers.fs
+++ src/Views/View.Organisers.fs
@@ -3,123 +3,80 @@ module App.Views.Organisers
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
+type Profile = {
+  Image : string
+  Tagline : string
+  Website : string
+  Twitter : string
+  Linkedin : string }
+
+let organiser name profile =
+  div [ Class "pt-4 m-4 bg-white card-hoverable flex flex-col items-stretch justify-between" ]
+     [ div [ Class "flex flex-col items-center justify-start" ]
+        [
+         div [ Class "text-grey-darkest text-sm text-center" ]
+          [ h3 [] [ str name ] ]
+
+         div [ Class "text-grey-darker text-xs italic text-center" ]
+          [ h4 [] [ str profile.Tagline ] ]
+         
+         div [ ]
+          [ img [ Src profile.Image ] ]
+        ]
+              
+       div [ Class "my-1 flex items-center justify-center" ]
+          [ a [ Class "website-icon"
+                Href profile.Website
+                Target "_blank"]
+              [ img [ Class "ml-2 h-10"
+                      Src "./img/website.png" ] ]
+            a [ Class "twitter-icon"
+                Href ("https://twitter.com/" + profile.Twitter )
+                Target "_blank"]
+              [ img [ Class "ml-2 h-10"
+                      Src "./img/twitter.png" ] ]
+            a [ Class "twitter-icon"
+                Href ("https://www.linkedin.com/in/"+ profile.Linkedin + "/")
+                Target "_blank"]
+              [ img [ Class "ml-2 h-10"
+                      Src "./img/linkedin.png" ] ]
+          ]
+      ]
+
 
 let organisers model dispatch =
   div [ Class "content"; Id "organisers" ]
-    [ div [ Class "my-8 w-4/5 lg:w-2/3 xl:w-1/2" ]
+    [ div [ Class "mt-8 w-4/5 lg:w-2/3 xl:w-1/2" ]
           [ h2 []
-                [ str "Organisers" ]
-            div [ Class "flex flex-col md:flex-row justify-between items-center mx-2" ]
+              [ str "Organisers" ]
+            div [ Class "flex flex-col md:flex-row justify-between items-center" ]
               [
-                div [ Class "m-2 bg-white w-5/6 md:w-1/3 rounded-lg shadow-lg md:p-8 mt-6" ]
-                   [ div [ Class "organiser text-center" ]
-                      [ h3 []
-                            [ str "Zsofia Herendi" ] ]
-                     div [ Class "organiser-picture" ]
-                      [ img [ Src "./img/zsofia.jpeg" ] ]
-                     div [ Class "organiser-name text-center" ]
-                      [ h4 []
-                            [ str "Flow addict PM" ] ]
-                     div [ Class "org-social" ]
-                        [ a [
-                            Class "website-icon"
-                            Href "https://www.zherendi.com/"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/website.png" ]
-                            ]
-                          a [
-                           Class "twitter-icon"
-                           Href "https://twitter.com/ZHerendi"
-                           Target "_blank"]
-                           [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/twitter.png" ]
-                            ]
-                          a [
-                            Class "twitter-icon"
-                            Href "https://www.linkedin.com/in/zsófia-herendi-2296b48/"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/linkedin.png" ]
-                            ]
-                      ]
-                    ]
-                    
-                div [ Class "m-2 bg-white w-5/6 md:w-1/3 rounded-lg shadow-lg md:p-8 mt-6" ]
-                   [ div [ Class "organiser text-center" ]
-                      [ h3 []
-                            [ str "Marco Heimeshoff" ] ]
-                     div [ Class "organiser-picture" ]
-                      [ img [ Src "./img/MarcoHeimeshoff.jpg" ] ]
-                     div [ Class "organiser-name text-center" ]
-                      [ h4 []
-                            [ str "Organiser of KanDDDinsky" ] ]
-                     div [ Class "org-social" ]
-                        [ a [
-                            Class "website-icon"
-                            Href "https://heimeshoff.de/"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/website.png" ]
-                            ]
-                          a [
-                            Class "twitter-icon"
-                            Href "https://twitter.com/Heimeshoff"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/twitter.png" ]
-                            ]
-                          a [
-                            Class "twitter-icon"
-                            Href "https://www.linkedin.com/in/heimeshoff/"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/linkedin.png" ]
-                            ] 
-                        ]
-                      ]  
-                div [ Class "m-2 bg-white w-5/6 md:w-1/3 rounded-lg shadow-lg md:p-8 mt-6" ]
-                   [ div [ Class "organiser text-center" ]
-                      [ h3 []
-                            [ str "Kenny Baas-Schwegler" ] ]
-                     div [ Class "organiser-picture" ]
-                      [ img [ Src "./img/kenny.jpg" ] ] 
-                     div [ Class "organiser-name text-center" ]
-                      [ h4 []
-                            [ str "Sociotechnical stoic" ] ]
-                     div [ Class "org-social" ]
-                        [ a [
-                            Class "website-icon"
-                            Href "https://baasie.com"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/website.png" ]
-                            ]
-                          a [
-                            Class "twitter-icon"
-                            Href "https://twitter.com/kenny_baas"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/twitter.png" ]
-                            ]
-                          a [
-                            Class "twitter-icon"
-                            Href "https://www.linkedin.com/in/kenny-baas/"
-                            Target "_blank"]
-                            [ img [
-                                Class "ml-2 h-10"
-                                Src "./img/linkedin.png" ]
-                            ] 
-                        ]
-                    ]
+                organiser "Zsofia Herendi"
+                  {
+                    Image = "./img/zsofia.jpeg" 
+                    Tagline = "Flow addict PM"
+                    Website = "https://www.zherendi.com/"
+                    Twitter = "ZHerendi"
+                    Linkedin = "zsófia-herendi-2296b48"
+                  }
+
+                organiser "Marco Heimeshoff"
+                  {
+                    Image = "./img/MarcoHeimeshoff.jpg"
+                    Tagline = "Business software artist"
+                    Website = "https://www.heimeshoff.de/"
+                    Twitter = "Heimeshoff"
+                    Linkedin = "heimeshoff"
+                  }
+                   
+                organiser "Kenny Baas-Schwegler"
+                  {
+                    Image = "./img/kenny.jpg"
+                    Tagline = "Sociotechnical stoic"
+                    Website = "https://baasie.com"
+                    Twitter = "kenny_baas"
+                    Linkedin = "kenny-baas"
+                  }
               ]   
           ]
     ]
