9ba5a2624dd0862eb88952a9986eb351337a826e
Marco Heimeshoff <heimeshoff@gmail.com>
2019-12-07 17:14:46 +0100

    The Navbar works differently now in mobile and desktop view

--- src/view/Navigation.fs
+++ src/view/Navigation.fs
@@ -6,74 +6,96 @@ module Navigation =
   open Fable.Helpers.React.Props
   open VDDD.Types
 
-  let menuitem_style = 
-    "px-2 py-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker"
+  let vddd_logo dispatch = 
+    div [ Class "p-4 cursor-pointer flex-no-shrink flex items-center justify-center rounded-lg hover:bg-grey-light"  
+          OnClick (fun _ -> GoTo (Landingpage "top") |> dispatch) ]
+      [ img [ Class "object-contain mr-2 h-8"
+              Src "./img/vddd_logo_tp.png" ] ]  
 
-  let entry label onClick =
-    div [ Class menuitem_style
-          OnClick (fun e -> e.stopPropagation() ; onClick() ) ]
-      [ label |> ofString ]    
-
-  let submenu label content =
-    button [ Class (sprintf "reveal-menu-content %s text-blue-dark focus:outline-none" menuitem_style) ]
-      [ label |> ofString
-        div [ Class "menu-content w-32"]
-          content ]
-
-  let links model dispatch =
-    div [ Class "flex flex-col lg:flex-row items-stretch justify-end" ]
-      [ entry "Sessions" (fun _ -> GoTo (Landingpage "sessions") |> dispatch)
-        entry "CFP" (fun _ -> GoTo (Landingpage "cfp") |> dispatch)
-        entry "Sponsors" (fun _ -> GoTo (Landingpage "sponsors") |> dispatch)
-        entry "About" (fun _ -> GoTo (Landingpage "about") |> dispatch)
-        submenu "Resources"
-          [ entry "Books" (fun _ -> GoTo Books |> dispatch)
-            entry "Podcasts" (fun _ -> GoTo Podcasts |> dispatch) ]
-
-        submenu "Social" 
-          [ a [ Class "w-full px-2 py-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker flex items-center justify-center"
+  let links render_entry render_submenu dispatch =
+    div [ Class "flex flex-col lg:flex-row items-start lg:items-stretch justify-end" ]
+      [ render_entry "Sessions" (fun _ -> GoTo (Landingpage "sessions") |> dispatch)
+        render_entry "CFP" (fun _ -> GoTo (Landingpage "cfp") |> dispatch)
+        render_entry "Sponsors" (fun _ -> GoTo (Landingpage "sponsors") |> dispatch)
+        render_entry "About" (fun _ -> GoTo (Landingpage "about") |> dispatch)
+        render_submenu "Resources"
+          [ render_entry "Books" (fun _ -> GoTo Books |> dispatch)
+            render_entry "Podcasts" (fun _ -> GoTo Podcasts |> dispatch) ]
+        render_submenu "Social" 
+          [ a [ Class "p-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker flex items-center justify-start lg:justify-center "
                 Href "https://t.co/MRxpx0rLH2?amp=1"
                 Target "_blank"]
               [ img [ Class "mr-2 h-6"
                       Src "./img/slack_icon.png" ] 
                 div [ Class "pt-1"] ["Slack " |> ofString ]
               ]
-            
-            a [ Class "w-full px-2 py-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker flex items-center justify-center"
+            a [ Class "p-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker flex items-center justify-start lg:justify-center"
                 Href "https://www.meetup.com/Virtual-Domain-Driven-Design-meetup/"
                 Target "_blank"]
               [ img [ Class "ml-2 h-6"
-                      Src "./img/meetup.svg" ] ]
-          ]
-      ]
+                      Src "./img/meetup.svg" ] ] ] ]
 
 
-  let render (model:Model) dispatch =
+  let entry_mobile label onClick =
+    div [ Class "w-full p-4 text-lg leading-tight cursor-pointer flex-no-shrink hover:bg-grey-light hover:text-blue-darker"
+          OnClick (fun e -> e.stopPropagation() ; onClick() ) ]
+      [ label |> ofString ]    
+
+  let submenu_mobile label content =
+    div [ Class "relative border-t border-grey-light w-full" ]
+      [ div [ Class "absolute pin-t pin-r text-grey pt-2 pr-4 text-md" ]
+          [ label |> ofString ]
+        div [ ]
+          content ]
+
+  let mobile_links model dispatch =
     let visibility =
       match model.menu_open with
       | true -> "block"
       | false -> "hidden"
-    
-    div [ Class "navbar" ]
-      [ div [ Class "lg:hidden navbar navbar-opaque " ]
-          [ div [ Class "w-full flex flex-row items-center justify-between" ]
-              [ div [ Class "p-4 font-bold text-lg cursor-pointer flex-no-shrink flex items-center justify-center rounded-lg hover:bg-grey-light hover:text-blue-darker"
-                      OnClick (fun _ -> GoTo (Landingpage "top") |> dispatch)]
-                    [ img [ Class "object-contain mr-2 h-8"
-                            Src "./img/vddd_logo_tp.png" ] ]
-                button [ Class "flex-no-shrink flex items-center m-4 px-3 py-2 border rounded border-white hover:text-blue-light hover:border-blue-light"
-                         OnClick (fun e -> e.stopPropagation() ; Toggle_menu |> dispatch ) ]
-                  [ svg [ Class "fill-current h-3 w-3" ; HTMLAttr.Custom ("viewBox", "0 0 20 20") ; HTMLAttr.Custom ("xmlns", "http://www.w3.org/2000/svg") ]
-                      [ path [ HTMLAttr.Custom ("d", "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z") ]
-                          [ ] ] ] ]
-                  
-            div [ Class (visibility + " bg-white w-full z-25") ]
-              [ links model dispatch] ]  
+    div [ Class (visibility + " bg-white w-full z-25 shadow-md") ]
+      [ links entry_mobile submenu_mobile dispatch]   
 
-        div [ Class ("navbar-permanent hidden lg:flex flex-row items-center justify-center navbar"); Id "navbar" ]
-          [ div [ Class "w-4/5 xl:w-2/3 flex flex-col lg:flex-row items-center justify-between" ]
-              [ div [ Class "p-4 font-bold text-lg cursor-pointer flex-no-shrink flex items-center justify-center rounded-lg hover:bg-grey-light hover:text-blue-darker"  
-                      OnClick (fun _ -> GoTo (Landingpage "top") |> dispatch) ]
-                    [ img [ Class "object-contain mr-2 h-8"
-                            Src "./img/vddd_logo_tp.png" ] ]
-                links model dispatch ] ] ]
+  let navbar_mobile dispatch = 
+    div [ Class "w-full flex flex-row items-center justify-between shadow-md" ]
+      [ div [ Class "p-4 font-bold text-lg cursor-pointer flex-no-shrink flex items-center justify-center rounded-lg hover:bg-grey-light hover:text-blue-darker"
+              OnClick (fun _ -> GoTo (Landingpage "top") |> dispatch)]
+            [ img [ Class "object-contain mr-2 h-8"
+                    Src "./img/vddd_logo_tp.png" ] ]
+        button [ Class "flex-no-shrink flex items-center m-4 px-3 py-2 border rounded border-white hover:text-blue-light hover:border-blue-light"
+                 OnClick (fun e -> e.stopPropagation() ; Toggle_menu |> dispatch ) ]
+          [ svg [ Class "fill-current h-3 w-3" ; HTMLAttr.Custom ("viewBox", "0 0 20 20") ; HTMLAttr.Custom ("xmlns", "http://www.w3.org/2000/svg") ]
+              [ path [ HTMLAttr.Custom ("d", "M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z") ]
+                  [ ] ] ] ]
+
+
+  let clickable_item_style = 
+    "px-2 py-4 text-lg leading-tight cursor-pointer flex-no-shrink rounded-lg hover:bg-grey-light hover:text-blue-darker"
+
+  let entry_desktop label onClick =
+    div [ Class clickable_item_style
+          OnClick (fun e -> e.stopPropagation() ; onClick() ) ]
+      [ label |> ofString ]    
+ 
+  let submenu_desktop label content =
+    button [ Class (sprintf "reveal-menu-content %s text-blue-dark focus:outline-none" clickable_item_style) ]
+      [ label |> ofString
+        div [ Class "menu-content"]
+          content ]
+
+  let navbar_desktop dispatch =
+    div [ Class "w-4/5 xl:w-2/3 flex items-center justify-between" ]
+      [ vddd_logo dispatch
+        links entry_desktop submenu_desktop dispatch ]       
+
+
+  let render (model:Model) dispatch =
+    div [ Class "navbar" ]
+      [ div [ Class "lg:hidden" ]
+          [ navbar_mobile dispatch
+            mobile_links model dispatch ]
+
+        div [Class "hidden lg:flex flex-row items-center justify-center"]
+          [ navbar_desktop dispatch ] ]
+
+  
