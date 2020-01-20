39d168e9bd3f818725ff700791711953b5ac00bb
Marco Heimeshoff <heimeshoff@gmail.com>
2020-01-20 11:36:47 +0100

    Added Social links into the hero

--- src/view/Hero.fs
+++ src/view/Hero.fs
@@ -6,6 +6,14 @@ module Hero =
   open Fable.Helpers.React.Props
   open VDDD.Types
 
+
+  let social_link label url =
+    a [ Class "text-xl m-2 p-2 text-white rounded-lg border-2 border-blue-500 hover:border-blue-400" 
+        Href url 
+        Target "_blank" ] 
+      [ label |> ofString ]
+ 
+
   let render model dispatch =
       div [ Class "hero flex flex-col items-center justify-center lg:flex-row-reverse lg:items-start"
             Style [ Background "url(./img/kandddinsky.jpg)"
@@ -13,9 +21,9 @@ module Hero =
                     BackgroundPosition "center" ] ]
         [ div [ Class "overlay" ] []
 
-          div [ Class "w-full lg:w-1/3 flex flex-col items-center justify-center" ]
+          div [ Class "w-full lg:w-1/3 flex flex-col items-center justify-center z-10" ]
             [
-              div [ Class "w-full p-4 sm:mt-8 sm:w-5/6 sm:rounded-lg sm:shadow-lg bg-white z-10 flex flex-col items-center justify-start"]
+              div [ Class "w-full p-4 sm:mt-8 sm:w-5/6 sm:rounded-lg sm:shadow-lg bg-white  flex flex-col items-center justify-start"]
                 [
                   img [ Class "hidden lg:block object-contain h-8 mb-4"
                         Src "./img/vddd_logo_tp.png" ]
@@ -27,6 +35,12 @@ module Hero =
                       Target "_blank"
                       Class "p-4 bg-blue-500 card-hoverable text-white" ]
                     [ str "Propose a session"]                      
+                ]
+              div [ Class "mt-4 flex items-center justify-center" ]
+                [
+                  social_link "Meetup" "https://www.meetup.com/Virtual-Domain-Driven-Design-meetup/"
+                  social_link "Slack" "https://t.co/MRxpx0rLH2?amp=1"
+                  social_link "Twitter" "https://twitter.com/VirtualDDD"
                 ]
             ]
 
@@ -41,7 +55,7 @@ module Hero =
                           ( s.Tail 
                             |> List.truncate 3
                             |> List.map Sessions.session_small )
-                        div [ Class "card-hoverable bg-white text-center cursor-pointer p-2 w-40 my-4"
+                        div [ Class "text-xl mb-4 p-2 cursor-pointer text-white rounded-lg border-2 border-blue-500 hover:border-blue-400"
                               OnClick (fun _ -> GoTo Sessions |> dispatch) ]
                           [ "Show all sessions" |> ofString ]
                       ]
