3b9406e84025f0c1c6f1fd0a03f12736f82b31df
Kenny Baas-Schwegler <kenny@baasie.com>
2020-08-09 13:59:22 +0200

    added pagination for podcasts

--- src/components/podcasts.tsx
+++ src/components/podcasts.tsx
@@ -1,6 +1,12 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement, useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronCircleLeft,
+  faChevronCircleRight,
+} from "@fortawesome/free-solid-svg-icons"
 
 const Podcast = ({ session }) => {
   if (session.podcast) {
@@ -38,6 +44,8 @@ const PodcastPlatform = ({ platform }) => {
 }
 
 const Podcasts = (): ReactElement => {
+  const pageLimit = 6
+  const [currentPage, setCurrentPage] = useState(1)
   const data = useStaticQuery(graphql`
     query {
       sessions: allContentYaml(
@@ -69,25 +77,93 @@ const Podcasts = (): ReactElement => {
       }
     }
   `)
+  const allPodcastsPlatforms = data.podcasts.nodes[0].podcastsPlatforms
+  const allPodcasts = data.sessions.nodes[0].sessions.filter(
+    (session) => session.podcast != null
+  )
+  const totalPodcastsLength = allPodcasts.length
+
+  const defineRightArrowVisibility = (newPage) => {
+    if (totalPodcastsLength > pageLimit * newPage) {
+      return ""
+    } else {
+      return "invisible"
+    }
+  }
+
+  const [leftInvisible, setLeftInvisible] = useState("invisible")
+  const [rightInvisible, setRightInvisible] = useState(
+    defineRightArrowVisibility(1)
+  )
+
+  const offset = (currentPage - 1) * pageLimit
+  const [currentPodcasts, setCurrentPodcasts] = useState(
+    allPodcasts.slice(offset, offset + pageLimit)
+  )
+
+  const handleMoveLeft = () => {
+    const newPage = currentPage - 1
+    const offset = (newPage - 1) * pageLimit
+    pageChange(offset, newPage)
+  }
+
+  const handleMoveRight = () => {
+    const newPage = currentPage + 1
+    const offset = (newPage - 1) * pageLimit
+    pageChange(offset, newPage)
+  }
+
+  const pageChange = (offset, newPage) => {
+    setCurrentPage(newPage)
+    setCurrentPodcasts(allPodcasts.slice(offset, offset + pageLimit))
+    if (newPage > 1) {
+      setLeftInvisible("")
+    } else {
+      setLeftInvisible("invisible")
+    }
+    setRightInvisible(defineRightArrowVisibility(newPage))
+  }
+
   return (
-    <div className="w-full flex flex-col items-center justify-start">
-      <h2 className="my-6 w-4/5 lg:w-2/3 xl:w-1/2">Podcasts</h2>
+    <div className="w-full flex flex-col items-center">
+      <h2 className="my-6 lg:w-2/3 xl:w-1/2">Podcasts</h2>
       <div>
         <p className="italic text-justify">
-          childImageSharp Listen to the VDDD podcast by clicking on one of the
-          platforms below
+          Listen to the VDDD podcast by clicking on one of the platforms below
         </p>
         <div className="my-1 w-full flex items-center justify-around">
-          {data.podcasts.nodes[0].podcastsPlatforms.map((platform, index) => {
+          {allPodcastsPlatforms.map((platform, index) => {
             return <PodcastPlatform platform={platform}></PodcastPlatform>
           })}
         </div>
       </div>
-      <div className="w-11/12 md:w-5/6">
-        <div className="flex items-stretch justify-center flex-wrap">
-          {data.sessions.nodes[0].sessions.map((session, index) => {
+      <div className="flex flex-row justify-center">
+        <div className="flex justify-center items-center w-1/20">
+          <button
+            onClick={handleMoveLeft}
+            className={
+              leftInvisible +
+              " transition duration-500 text-blue-700 hover:text-blue-400"
+            }
+          >
+            <FontAwesomeIcon icon={faChevronCircleLeft} size="4x" />
+          </button>
+        </div>
+        <div className="flex flex-row flex-wrap items-center w18/20">
+          {currentPodcasts.map((session, index) => {
             return <Podcast session={session}></Podcast>
           })}
+        </div>
+        <div className="flex justify-center items-center w-1/20">
+          <button
+            onClick={handleMoveRight}
+            className={
+              rightInvisible +
+              " transition duration-500 text-blue-700 hover:text-blue-400"
+            }
+          >
+            <FontAwesomeIcon icon={faChevronCircleRight} size="4x" />
+          </button>
         </div>
       </div>
     </div>
