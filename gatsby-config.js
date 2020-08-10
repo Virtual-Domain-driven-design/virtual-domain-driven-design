c2d6981464fd1fa52a0dab7cd8e6219b4d1ea731
Kenny Baas-Schwegler <kenny@baasie.com>
2020-08-10 12:14:23 +0200

    added gatsby offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -168,8 +168,11 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/learning-ddd/`, `/learning-ddd/*`],
+      },
+    },
   ],
 }
