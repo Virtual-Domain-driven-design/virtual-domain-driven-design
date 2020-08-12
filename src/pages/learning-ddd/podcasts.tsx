cab4211b914231202db1fe3b3b6a73b46a4acbbd
Kenny Baas-Schwegler <kenny@baasie.com>
2020-08-12 15:16:49 +0200

    update podcasts

--- src/pages/learning-ddd/podcasts.tsx
+++ src/pages/learning-ddd/podcasts.tsx
@@ -1,13 +1,22 @@
-import React, { ReactElement } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import Layout from "../../templates/layout"
+import React, { FC } from "react"
 
+import Layout from "../../templates/layout"
 import Podcast from "../../components/podcast"
 import PodcastPlatforms from "../../components/podcast-platforms"
 import SEO from "../../components/seo"
 
-function Podcasts(): ReactElement {
-  const data = useStaticQuery(graphql`
+interface Podcast {
+  title: string
+  level: string
+  podcast: string
+  tags: string[]
+}
+
+const Podcasts: FC = () => {
+  const podcasts = useStaticQuery<{
+    allContentYaml: { nodes: { sessions: Podcast[] }[] }
+  }>(graphql`
     query {
       sessions: allContentYaml(
         filter: { sessions: { elemMatch: { title: { ne: null } } } }
@@ -19,27 +28,8 @@ function Podcasts(): ReactElement {
           }
         }
       }
-      podcasts: allContentYaml(
-        filter: { podcastsPlatforms: { elemMatch: { name: { ne: null } } } }
-      ) {
-        nodes {
-          podcastsPlatforms {
-            name
-            url
-            img {
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
     }
-  `)
-  const podcastsPlatforms = data.podcasts.nodes[0].podcastsPlatforms
-  const podcasts = data.sessions.nodes[0].sessions.filter(
+  `).allContentYaml.nodes[0].sessions.filter(
     (session) => session.podcast != null
   )
 
@@ -53,21 +43,7 @@ function Podcasts(): ReactElement {
       />
       <div className="w-full flex flex-col items-center">
         <h2 className="my-6 lg:w-2/3 xl:w-1/2">Podcasts</h2>
-        <div>
-          <p className="italic text-justify">
-            Listen to the VDDD podcast by clicking on one of the platforms below
-          </p>
-          <div className="my-1 w-full flex items-center justify-around">
-            {podcastsPlatforms.map((platform, index) => {
-              return (
-                <PodcastPlatforms
-                  key={index}
-                  platform={platform}
-                ></PodcastPlatforms>
-              )
-            })}
-          </div>
-        </div>
+        <PodcastPlatforms />
         <div className="flex flex-row justify-center">
           <div className="flex flex-row flex-wrap items-center w18/20">
             {podcasts.map((session, index) => {
