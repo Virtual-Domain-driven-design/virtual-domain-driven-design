78ced8f0c1f245d7fc5e8cf5b7cf1291bbe7976f
Kenny Baas-Schwegler <kenny@baasie.com>
2020-12-28 14:23:59 +0100

    twin macro migration conferences

--- src/components/conferences.tsx
+++ src/components/conferences.tsx
@@ -1,27 +1,28 @@
-import React, { ReactElement } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import React, { ReactElement } from "react"
+import "twin.macro"
 
 const Conference = ({ name, image, location, date, website }) => {
   return (
     <a
       key={name}
-      className="group floating-action-button bg-white w-full sm:w-64 rounded-lg shadow-md m-2 flex flex-col items-center justify-start"
+      tw="flex justify-center "
       href={website}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="flex flex-col items-center justify-start">
-        <div className="m-2 h-8 font-semibold text-gray-800 text-sm text-center">
+      <div tw="bg-white w-full sm:w-64 shadow-md rounded-lg m-4 flex flex-col items-center justify-between transform scale-100 duration-100 hover:scale-110">
+        <div tw="m-2 font-semibold text-gray-800 text-sm text-center">
           {name}
         </div>
-        <div className="text-gray-800 text-sm text-center">{date}</div>
+        <div tw="text-gray-800 text-sm text-center">{date}</div>
         <Img
           fluid={image}
-          className="my-2 w-64 h-32"
+          tw="my-2 w-64 h-32"
           imgStyle={{ objectFit: "contain" }}
         />
-        <div className="text-gray-800 text-sm text-center">{location}</div>
+        <div tw="mb-2 text-gray-800 text-sm text-center">{location}</div>
       </div>
     </a>
   )
@@ -58,43 +59,60 @@ const Conferences = (): ReactElement => {
           }
         }
       }
+      dddtaiwan: file(relativePath: { eq: "logo/ddd-taiwan.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
   return (
-    <div className="section bg-gray-200" id="conferencess">
-      <div className="w-full flex flex-col items-center justify-start">
-        <h2 className="my-6 w-4/5 lg:w-2/3 xl:w-1/2">Conferences</h2>
-        <div className="md:w-5/6">
-          <div className="flex justify-center flex-wrap">
-            <Conference
-              name="Domain-Driven Design Europe"
-              image={data.dddeu.childImageSharp.fluid}
-              location="Online-only"
-              date="February (Online)"
-              website="https://dddeurope.com/"
-            ></Conference>
-            <Conference
-              name="Explore DDD"
-              image={data.eddd.childImageSharp.fluid}
-              location="Keystone, Colorado, USA"
-              date="November (Virtual)"
-              website="https://exploreddd.com/"
-            ></Conference>
-            <Conference
-              name="KanDDDinsky"
-              image={data.kddd.childImageSharp.fluid}
-              location="Berlin, Germany"
-              date="2021"
-              website="https://kandddinsky.de/"
-            ></Conference>
-            <Conference
-              name="DDD China"
-              image={data.dddchina.childImageSharp.fluid}
-              location="Beijing, China"
-              date="November"
-              website="http://ddd-china.com/"
-            ></Conference>
-          </div>
+    <div
+      tw="bg-white flex flex-col items-center justify-center m-4"
+      id="conferencess"
+    >
+      <h2 tw="my-2 w-4/5 lg:w-2/3 xl:w-1/2 text-blue-800 text-3xl">
+        Conferences
+      </h2>
+      <div tw="md:w-5/6">
+        <div tw="flex justify-center flex-wrap">
+          <Conference
+            name="Domain-Driven Design Europe"
+            image={data.dddeu.childImageSharp.fluid}
+            location="Online-only"
+            date="February 4-5, 2021"
+            website="https://dddeurope.com/"
+          ></Conference>
+          <Conference
+            name="Explore DDD"
+            image={data.eddd.childImageSharp.fluid}
+            location="Keystone, Colorado, USA"
+            date="November (Virtual)"
+            website="https://exploreddd.com/"
+          ></Conference>
+          <Conference
+            name="KanDDDinsky"
+            image={data.kddd.childImageSharp.fluid}
+            location="Berlin, Germany"
+            date="2021"
+            website="https://kandddinsky.de/"
+          ></Conference>
+          <Conference
+            name="DDD China"
+            image={data.dddchina.childImageSharp.fluid}
+            location="Beijing, China"
+            date="November"
+            website="http://ddd-china.com/"
+          ></Conference>
+          <Conference
+            name="DDD Taiwan"
+            image={data.dddtaiwan.childImageSharp.fluid}
+            location="Taipei, Taiwan"
+            date="November"
+            website="https://www.ddd-tw.com/"
+          ></Conference>
         </div>
       </div>
     </div>
