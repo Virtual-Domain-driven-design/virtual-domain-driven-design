3e215968c02d14782d3da61de12600013f01782e
Kenny Baas-Schwegler <kenny@baasie.com>
2020-12-29 16:55:00 +0100

    upgrade tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
-const tailwindcss = require(`tailwindcss`)
-
 module.exports = {
-  plugins: [
-    tailwindcss(`./tailwind.config.js`),
-    require(`autoprefixer`),
-    require(`cssnano`)({
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {
       preset: `default`,
-    }),
-  ],
+    },
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       sans: [
