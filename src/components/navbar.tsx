06f1bc2645e08ea68d807bc3560ccb669bf63282
Kenny Baas-Schwegler <kenny@baasie.com>
2021-01-02 11:58:13 +0100

    navbar twin.macro migration

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
-import React, { ReactElement, useState } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import React, { ReactElement, useState } from "react"
+import tw from "twin.macro"
 
 import MeetupSvg from "../images/logo/meetup.svg"
 
@@ -10,11 +11,11 @@ const VDDDLogo = ({ data }) => {
   return (
     <Link
       to="/"
-      className="p-4 cursor-pointer flex-shrink-0 flex items-center justify-center rounded-lg hover:bg-gray-400"
+      tw="p-4 cursor-pointer flex-shrink-0 flex items-center justify-center rounded-lg hover:bg-gray-400"
     >
       <Img
         fixed={data.vdddLogoTp.childImageSharp.fixed}
-        className="object-contain mr-2 h-8"
+        tw="object-contain mr-2 h-8"
       />
     </Link>
   )
@@ -22,9 +23,12 @@ const VDDDLogo = ({ data }) => {
 
 const SocialMenu = ({ data }) => {
   return (
-    <button className="reveal-menu-content px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none">
+    <button
+      className="group"
+      tw="relative px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none"
+    >
       Socials
-      <div className="menu-content">
+      <div tw="absolute top-0 right-0 mt-14 w-32 bg-white rounded shadow-lg z-30 hidden group-hover:block">
         <SocialSubItems data={data}></SocialSubItems>
       </div>
     </button>
@@ -33,10 +37,8 @@ const SocialMenu = ({ data }) => {
 
 const SocialMobileMenu = ({ data }) => {
   return (
-    <div className="relative border-t border-gray-400 w-full">
-      <div className="absolute top-0 right-0 text-gray-500 pt-2 pr-4 text-md">
-        Socials
-      </div>
+    <div tw="relative border-t border-gray-400 w-full">
+      <div tw="absolute top-0 right-0 text-gray-500 pt-2 pr-4">Socials</div>
       <SocialSubItems data={data}></SocialSubItems>
     </div>
   )
@@ -46,35 +48,29 @@ const SocialSubItems = ({ data }) => {
   return (
     <div>
       <a
-        className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+        tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
         href="https://www.meetup.com/Virtual-Domain-Driven-Design-meetup/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <MeetupSvg className="mr-2 h-8" />
+        <MeetupSvg tw="mr-2 h-8" />
       </a>
       <a
-        className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+        tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
         href="https://j.mp/ddd-es-cqrs"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Img
-          fixed={data.slackLogo.childImageSharp.fixed}
-          className="mr-2 h-8"
-        />
+        <Img fixed={data.slackLogo.childImageSharp.fixed} tw="mr-2 h-8" />
         Slack
       </a>
       <a
-        className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+        tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
         href="https://twitter.com/VirtualDDD"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Img
-          fixed={data.twitterLogo.childImageSharp.fixed}
-          className="mr-2 h-8"
-        />
+        <Img fixed={data.twitterLogo.childImageSharp.fixed} tw="mr-2 h-8" />
         twitter
       </a>
     </div>
@@ -83,9 +79,12 @@ const SocialSubItems = ({ data }) => {
 
 const ContributionMenu = ({ data }) => {
   return (
-    <button className="reveal-menu-content px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none">
+    <button
+      className="group"
+      tw="relative px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none"
+    >
       Contribute
-      <div className="menu-content">
+      <div tw="absolute top-0 right-0 mt-14 w-32 bg-white rounded shadow-lg z-30 hidden group-hover:block">
         <ContributionSubItems data={data}></ContributionSubItems>
       </div>
     </button>
@@ -94,10 +93,8 @@ const ContributionMenu = ({ data }) => {
 
 const ContributionMobileMenu = ({ data }) => {
   return (
-    <div className="relative border-t border-gray-400 w-full">
-      <div className="absolute top-0 right-0 text-gray-500 pt-2 pr-4 text-md">
-        Contribute
-      </div>
+    <div tw="relative border-t border-gray-400 w-full">
+      <div tw="absolute top-0 right-0 text-gray-500 pt-2 pr-4">Contribute</div>
       <ContributionSubItems data={data}></ContributionSubItems>
     </div>
   )
@@ -107,27 +104,21 @@ const ContributionSubItems = ({ data }) => {
   return (
     <div>
       <a
-        className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+        tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
         href="https://github.com/Virtual-Domain-driven-design/virtual-domain-driven-design"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Img
-          fixed={data.githubLogo.childImageSharp.fixed}
-          className="mr-2 h-8"
-        />
+        <Img fixed={data.githubLogo.childImageSharp.fixed} tw="mr-2 h-8" />
         Github
       </a>
       <a
-        className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+        tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
         href="https://virtualddd.com/admin"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Img
-          fixed={data.netlifyLogo.childImageSharp.fixed}
-          className="mr-2 h-8"
-        />
+        <Img fixed={data.netlifyLogo.childImageSharp.fixed} tw="mr-2 h-8" />
         Netlify CMS
       </a>
     </div>
@@ -138,7 +129,7 @@ const DesktopNavigationItem = ({ to, label }) => {
   return (
     <Link
       to={to}
-      className="reveal-menu-content px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none"
+      tw="relative px-2 py-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 text-blue-600 focus:outline-none"
     >
       {label}
     </Link>
@@ -149,7 +140,7 @@ const MobileNavigationItem = ({ to, label }) => {
   return (
     <Link
       to={to}
-      className="w-full p-4 text-lg leading-tight cursor-pointer flex-shrink-0 hover:bg-gray-400 hover:text-blue-700"
+      tw="w-full p-4 text-lg leading-tight cursor-pointer flex-shrink-0 hover:bg-gray-400 hover:text-blue-700"
     >
       {label}
     </Link>
@@ -158,24 +149,17 @@ const MobileNavigationItem = ({ to, label }) => {
 
 const NavbarMobile = ({ data }) => {
   const [isMenuOpen, setMenuOpen] = useState(false)
-  const visibility = () => {
-    if (isMenuOpen) {
-      return "block bg-white w-full z-25 shadow-md"
-    }
-    return "hidden bg-white w-full z-25 shadow-md"
-  }
+
   return (
-    <div className="lg:hidden">
-      <div className="w-full flex flex-row items-center justify-between shadow-md">
+    <div tw="lg:hidden">
+      <div tw="w-full flex flex-row items-center justify-between shadow-md">
         <VDDDLogo data={data}></VDDDLogo>
         <button
-          className="reveal-menu-content flex-shrink-0 flex items-center m-4 px-3 py-2 border rounded border-white hover:text-blue-400 hover:border-blue-400"
-          onClick={() => {
-            setMenuOpen((isMenuOpen) => !isMenuOpen)
-          }}
+          tw="relative flex-shrink-0 flex items-center m-4 px-3 py-2 border rounded border-white hover:text-blue-400 hover:border-blue-400"
+          onClick={() => setMenuOpen((isMenuOpen) => !isMenuOpen)}
         >
           <svg
-            className="fill-current h-3 w-3"
+            tw="fill-current h-3 w-3"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -185,8 +169,11 @@ const NavbarMobile = ({ data }) => {
         </button>
       </div>
 
-      <div className={visibility()}>
-        <div className="flex flex-col lg:flex-row items-start lg:items-stretch justify-end">
+      <div
+        tw="block bg-white w-full z-30 shadow-md"
+        css={!isMenuOpen && tw`invisible`}
+      >
+        <div tw="flex flex-col lg:flex-row items-start lg:items-stretch justify-end">
           <MobileNavigationItem
             to="/sessions"
             label="Sessions"
@@ -200,7 +187,7 @@ const NavbarMobile = ({ data }) => {
             label="Conference"
           ></MobileNavigationItem>
           <a
-            className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+            tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
             href="https://dddheuristics.com"
             target="_blank"
             rel="noopener noreferrer"
@@ -217,9 +204,9 @@ const NavbarMobile = ({ data }) => {
 
 const NavbarDesktop = ({ data }) => {
   return (
-    <div className="w-4/5 xl:w-2/3 flex items-center justify-between">
+    <div tw="w-4/5 xl:w-2/3 flex items-center justify-between">
       <VDDDLogo data={data}></VDDDLogo>
-      <div className="flex flex-col lg:flex-row items-start lg:items-stretch justify-end">
+      <div tw="flex flex-col lg:flex-row items-start lg:items-stretch justify-end">
         <DesktopNavigationItem
           to="/sessions"
           label="Sessions"
@@ -233,7 +220,7 @@ const NavbarDesktop = ({ data }) => {
           label="Conference"
         ></DesktopNavigationItem>
         <a
-          className="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
+          tw="p-4 text-lg leading-tight cursor-pointer flex-shrink-0 rounded-lg hover:bg-gray-400 hover:text-blue-700 flex items-center justify-start lg:justify-center"
           href="https://dddheuristics.com"
           target="_blank"
           rel="noopener noreferrer"
@@ -289,9 +276,9 @@ const NavBar = (): ReactElement => {
   `)
 
   return (
-    <div className="navbar">
+    <div tw="bg-white shadow-md text-blue-600 h-16 sticky top-0 inset-x-0 z-50">
       <NavbarMobile data={data}></NavbarMobile>
-      <div className="hidden lg:flex flex-row items-center justify-center">
+      <div tw="hidden lg:flex flex-row items-center justify-center">
         <NavbarDesktop data={data}></NavbarDesktop>
       </div>
     </div>
