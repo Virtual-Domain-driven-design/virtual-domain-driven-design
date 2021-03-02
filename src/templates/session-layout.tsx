a5b286801135ca51af9ac4a031d446c1190def77
Kenny Baas-Schwegler <kenny@baasie.com>
2021-03-02 14:01:21 +0100

    Filtered out upcomingSession from #30 for now

--- src/templates/session-layout.tsx
+++ src/templates/session-layout.tsx
@@ -13,65 +13,62 @@ import Layout from "./layout"
 import SEO from "../components/seo"
 
 const SessionLayout: FC = ({ pageContext, data }) => {
-  let session = null
   if (data.sessionsYaml) {
-    session = data.sessionsYaml.sessions.find(
+    const session = data.sessionsYaml.sessions.find(
       (session) => session.id === pageContext.id
+    )
+    const keywords = session.tags.join(", ")
+    return (
+      <Layout>
+        <SEO
+          title={session.title}
+          description={session.description}
+          keywords={keywords}
+        />
+        <div tw="  flex flex-col items-center">
+          <div tw="bg-white w-2/3 rounded-lg shadow-md p-2 m-2 flex flex-col">
+            <div tw="text-sm text-gray-600">{session.date}</div>
+            <div css={[{ paddingTop: "56.25%" }, tw`relative`]}>
+              <iframe
+                title={session.title}
+                tw="absolute top-0 left-0 w-full h-full"
+                allowFullScreen={true}
+                src={session.video}
+                scrolling="no"
+                frameBorder={0}
+              ></iframe>
+            </div>
+            <a
+              tw="text-sm text-left font-bold text-blue-600 hover:cursor-pointer hover:text-blue-400"
+              href={session.video}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {session.title}
+            </a>
+            <div tw="px-1 w-full flex flex-row flex-wrap">
+              <div tw="flex-shrink-0 leading-none text-xs tracking-tighter bg-blue-700 text-white rounded-md p-1 m-1">
+                Level: {session.level}
+              </div>
+              {session.tags.map((tag, index) => {
+                return (
+                  <div
+                    key={index}
+                    tw="flex-shrink-0 leading-none text-xs tracking-tighter bg-gray-200 text-gray-700 rounded-md p-1 m-1"
+                  >
+                    {tag}
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </Layout>
     )
   }
   if (data.upcomingSessionsYaml) {
-    session = data.upcomingSessionsYaml.upcomingSessions.find(
-      (upcomingSession) => upcomingSession.id === pageContext.id
-    )
+    return <Layout>Upcoming on the way</Layout>
   }
-  const keywords = session.tags.join(", ")
-  return (
-    <Layout>
-      <SEO
-        title={session.title}
-        description={session.description}
-        keywords={keywords}
-      />
-      <div tw="  flex flex-col items-center">
-        <div tw="bg-white w-2/3 rounded-lg shadow-md p-2 m-2 flex flex-col">
-          <div tw="text-sm text-gray-600">{session.date}</div>
-          <div css={[{ paddingTop: "56.25%" }, tw`relative`]}>
-            <iframe
-              title={session.title}
-              tw="absolute top-0 left-0 w-full h-full"
-              allowFullScreen={true}
-              src={session.video}
-              scrolling="no"
-              frameBorder={0}
-            ></iframe>
-          </div>
-          <a
-            tw="text-sm text-left font-bold text-blue-600 hover:cursor-pointer hover:text-blue-400"
-            href={session.video}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {session.title}
-          </a>
-          <div tw="px-1 w-full flex flex-row flex-wrap">
-            <div tw="flex-shrink-0 leading-none text-xs tracking-tighter bg-blue-700 text-white rounded-md p-1 m-1">
-              Level: {session.level}
-            </div>
-            {session.tags.map((tag, index) => {
-              return (
-                <div
-                  key={index}
-                  tw="flex-shrink-0 leading-none text-xs tracking-tighter bg-gray-200 text-gray-700 rounded-md p-1 m-1"
-                >
-                  {tag}
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
 }
 
 export default SessionLayout
