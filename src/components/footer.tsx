4567b8a85f02b442897e59714a4804da690cdd44
Kenny Baas-Schwegler <kenny@baasie.com>
2021-03-03 17:39:10 +0100

    updating the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,15 @@ const Footer: FC = () => {
   return (
     <div tw="bg-white flex flex-col items-center justify-center p-4  text-center">
       <div>Copyright © Virtual Domain-Driven Design</div>
-      <div>Developed by Kenny Baas-Schwegler &amp; Marco Heimeshoff </div>
+      <div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Virtual-Domain-driven-design/virtual-domain-driven-design/graphs/contributors"
+        >
+          Developed by Virtual Domain-Driven Design Community
+        </a>
+      </div>
       <Link to="/code-of-conduct" className="link p-2">
         Code of conduct
       </Link>
