27eeda8ee52c68effe61f6ed792a563cf0671bea
yellowbrickc <katihirth@gmail.com>
2021-04-07 11:28:05 +0200

    #33 Add snapshot tests for the slack, twitter and VDDD logos

--- src/components/level-filter.test.tsx
+++ src/components/level-filter.test.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import * as React from "react"
 import renderer from "react-test-renderer"
 import "jest-styled-components"
 import { fireEvent, render } from "@testing-library/react"
